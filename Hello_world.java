7c4c9ce97e49eb16275d4d32c31249bb829f9788
Kokos09 <33238740+Kokos09@users.noreply.github.com>
2018-03-14 19:35:39 -0600

    Add files via upload
    A regular hello world

---
+++ Hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hernan
+ */
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo XD");
+    }
+    
+}
